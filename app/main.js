ad993fc660be5f9f4b4942dc87b549865e60c7f0
Jason Frame <jason@onehackoranother.com>
2014-06-12 18:53:02 +0100

    bootup

--- app/main.js
+++ app/main.js
@@ -1,17 +1,24 @@
-
 global.oblib = function(lib) { return require('./lib/' + lib); };
 global.obapi = function(api) { return require('./api/' + api); };
 
+process.on('uncaughtException', function(err) {
+    console.error("!!! UNHANDLED EXCEPTION !!!");
+    console.error(err);
+});
+
 exports.init = function(window, document) {
+
+	global.window	= window;
+	global.document	= document;
 
 	var signals = oblib('signals');
 
 	signals.register('taskRegistered');
 	signals.register('taskSpawned');
 
-	var kernel = oblib('kernel');
+	// var kernel = oblib('kernel');
 
-	var wsm = kernel.spawn('obie.workspace-manager');
+	// var wsm = kernel.spawn('obie.workspace-manager');
 	// TODO: present wsm.view to the view engine
 	
 }
