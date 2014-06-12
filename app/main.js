7fb35a52d2b546d050ecd8088f2ce1b091ab1cbf
Jason Frame <jason@onehackoranother.com>
2014-06-12 19:16:10 +0100

    error reporting

--- app/main.js
+++ app/main.js
@@ -1,20 +1,26 @@
 global.oblib = function(lib) { return require('./lib/' + lib); };
 global.obapi = function(api) { return require('./api/' + api); };
 
-process.on('uncaughtException', function(err) {
-    console.error("!!! UNHANDLED EXCEPTION !!!");
-    console.error(err);
-});
+function loadPlugin(mod) {
+	mod.init();
+}
 
 exports.init = function(window, document) {
 
 	global.window	= window;
 	global.document	= document;
 
+	//
+	//
+
+	loadPlugin(require('./plugins/error_reporting'));
+
 	var signals = oblib('signals');
 
 	signals.register('taskRegistered');
 	signals.register('taskSpawned');
+
+	throw new Error("broken broken!");
 
 	// var kernel = oblib('kernel');
 
---
+++ app/plugins/error_reporting.js
@@ -0,0 +1,35 @@
+var errorContainer 	= null;
+
+exports.init = function() {
+
+	errorContainer = document.createElement('div');
+	errorContainer.id = 'ob-error-report';
+	errorContainer.style.display = 'none';
+	document.body.appendChild(errorContainer);
+
+	function showError(el) {
+		errorContainer.appendChild(el);
+		errorContainer.style.display = 'block';
+	}
+
+	process.on('uncaughtException', function(err) {
+
+		var div = document.createElement('div');
+		div.className = 'ob-error-report-item';
+		
+		div.innerHTML = [
+			'<h2>Unhandled error</h2>',
+			'<h3>' + err.message + '</h3>'
+		].join("\n");
+
+		var stack = document.createElement('pre');
+		stack.textContent = err.stack;
+		div.appendChild(stack);
+
+		div.error = err;
+
+		showError(div);
+
+	});
+
+}
